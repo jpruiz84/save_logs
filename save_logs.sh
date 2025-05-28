01364290a6fd80ac22db09d1bbcbbfcc369230c4
Juan Pablo Ruiz-Rosero <jpruiz84@gmail.com>
2025-05-28 10:23:28 +0300

    Add dtb

--- save_logs.sh
+++ save_logs.sh
@@ -18,6 +18,7 @@ cat /proc/interrupts > logs_$1_interrupts.txt
 sort /proc/modules > logs_$1_modules.txt
 sort <(lsmod) > logs_$1_lsmod.txt
 dtc -I fs -O dts /sys/firmware/devicetree/base > logs_$1_dtb.dts
+cat /sys/firmware/devicetree/base > logs_$1_dtb.dtb
 cat /var/log/Xorg.0.log > logs_$1_xorg.txt
 ls -lah /sys/bus/platform/devices/ > logs_$1_devices.txt
 cat /sys/kernel/debug/tegra-host1x/devices > logs_$1_tegra-host1x.txt
