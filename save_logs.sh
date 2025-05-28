9221e896489ff4a212efca554eae3f4134918f2d
Juan Pablo Ruiz-Rosero <jpruiz84@gmail.com>
2025-05-28 10:34:27 +0300

    Fix dtb, removed host1x

--- save_logs.sh
+++ save_logs.sh
@@ -18,11 +18,9 @@ cat /proc/interrupts > logs_$1_interrupts.txt
 sort /proc/modules > logs_$1_modules.txt
 sort <(lsmod) > logs_$1_lsmod.txt
 dtc -I fs -O dts /sys/firmware/devicetree/base > logs_$1_dtb.dts
-cat /sys/firmware/devicetree/base > logs_$1_dtb.dtb
+cp /sys/firmware/fdt logs_$1_dtb.dtb
 cat /var/log/Xorg.0.log > logs_$1_xorg.txt
 ls -lah /sys/bus/platform/devices/ > logs_$1_devices.txt
-cat /sys/kernel/debug/tegra-host1x/devices > logs_$1_tegra-host1x.txt
-cat /sys/kernel/debug/tegra-host1x/status_all >> logs_$1_tegra-host1x.txt
 journalctl -b0 > logs_$1_journalctl.txt
 zcat /proc/config.gz > logs_$1_kernel_configs.txt
 tree /sys > logs_$1_tree_sys.txt
